6ea2464c6dda4f6867c4d61c3c03f0a59198f407
alish-ctrl <aliyaumetalieva2@gmail.com>
2026-06-27 16:21:03 +0600

    Написала код в Home.jsx

---
+++ site/src/pages/Home.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Home(){
+    return(
+        <div className="home">
+            <h1>Добро пожаловать в мир космос!</h1>
+            <p>
+                Космос — это бескрайнее пространство,
+                 наполненное звёздами,
+        планетами, галактиками и множеством удивительных 
+        объектов.
+            </p>
+
+            <p>   
+        На этом сайте вы сможете узнать
+        больше о Солнечной системе,
+        звёздах, галактиках и самых известных
+         космических миссиях.
+            </p>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsXIBUwVTLDj77raUUc4Ok7dv_QlWujQPk9uwTfwJmGDJYuLyQu3FFJYY&s=10"
+            alt="Космос" width="600px"></img>
+        </div>
+    )
+}
+export default Home;
