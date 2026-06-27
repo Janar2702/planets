7087c52c3bc8a862cfc5f9c65235a899a1013161
alish-ctrl <aliyaumetalieva2@gmail.com>
2026-06-27 16:29:56 +0600

    Создала еще одну отдельную страницу с мини-тестом

--- site/src/components/Navbar.jsx
+++ site/src/components/Navbar.jsx
@@ -10,7 +10,9 @@ function Navbar(){
                 <li><Link to="/Planets">Планеты</Link></li>
                 <li><Link to="/Stars">Звезды</Link></li>
                 <li><Link to="/Galaxies">Галактики</Link></li>
+                <li><Link to="/Missions">Миссии</Link></li>
                 <li><Link to="/About">О проекте</Link></li>
+                <li><Link to="/Test">Мини тест</Link></li>
             </ul>
         </nav>
     )
