0fe1b4bc39de1ce819eae56c0d2df454bfff8d71
alish-ctrl <aliyaumetalieva2@gmail.com>
2026-06-27 15:47:49 +0600

    Добавила компоненты и код в Navbar

--- site/src/components/Navbar.jsx
+++ site/src/components/Navbar.jsx
@@ -2,19 +2,17 @@ import {Link} from "react-router-dom";
 import "./Navbar.css";
 
 function Navbar(){
-    return (
+    return(
         <nav className="navbar">
-    <h2 className="logo">Космос</h2>
-
-    <ul className="navlinks">
-        <li><Link to="/">Главная</Link></li>
-        <li><Link to="/Planets">Планеты</Link></li>
-        <li><Link to="/Stars">Звезды</Link></li>
-        <li><Link to="/Galaxies">Галактики</Link></li>
-        <li><Link to="/About"></Link></li>
-    </ul>
-</nav>
-    );
+            <h2 className="theme">Космос</h2>
+            <ul className="nav-links">
+                <li><Link to ="/">Главная</Link></li>
+                <li><Link to="/Planets">Планеты</Link></li>
+                <li><Link to="/Stars">Звезды</Link></li>
+                <li><Link to="/Galaxies">Галактики</Link></li>
+                <li><Link to="/About">О проекте</Link></li>
+            </ul>
+        </nav>
+    )
 }
-
 export default Navbar;
