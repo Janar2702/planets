64799bcd2212c2aabf73908f2d659b0eaa982270
alish-ctrl <aliyaumetalieva2@gmail.com>
2026-07-03 12:48:32 +0600

    добавила в app.jsx функцию quiz

--- site/src/App.jsx
+++ site/src/App.jsx
@@ -4,6 +4,7 @@ import Planets from './pages/Planets'
 import About from './pages/About'
 import Home  from './pages/Home'
 import Navbar from './components/Navbar'
+import Quiz from './pages/Test'
 import './App.css'
 
 function App() {
